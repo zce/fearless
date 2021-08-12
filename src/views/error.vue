3eda0b5e82c1e7b06037ea659acd3c4496abeda7
zce <w@zce.me>
2021-08-12 12:03:19 +0800

    feat: initial commit

--- src/views/error.vue
+++ src/views/error.vue
@@ -1,15 +1,23 @@
-<!-- Oh no! -->
-
 <template>
-  <n-result status="500" title="Server Error" description="Server error may prove that you need hiring more developers.">
+  <n-result
+    status="404"
+    title="404 Not Found"
+    description="You know life is always ridiculous."
+    size="huge"
+  >
     <template #footer>
-      <n-button>Speard Money Out</n-button>
+      <n-button @click="router.replace('/')">Back to Home →</n-button>
     </template>
   </n-result>
 </template>
 
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
+
 <style scoped>
-  .n-result {
-    margin: 20vh auto 0;
-  }
-  </style>
+.n-result {
+  margin: 20vh auto 0;
+}
+</style>
