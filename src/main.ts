43d51f69adde8cbdb123429816613014f35e6f74
zce <w@zce.me>
2021-09-09 17:39:38 +0800

    feat: import naive on demand

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { createApp } from 'vue'
-import naive from 'naive-ui'
 import router from './router'
 import store from './store'
+import naive from './naive'
 import App from './app.vue'
 
 const app = createApp(App)
 
-app.use(naive)
 app.use(router)
 app.use(store)
+app.use(naive)
 
 app.mount('#app')
